a182a2c90f7af2124e6028428d87fe23ad4637cf
TonyTor <Tony.Tor2016@yandex.ru>
2020-03-11 15:03:07 +0400

    Create autorization

--- src/main/java/com/example/demo/autorizationandautofication/JwtUserDetailsService.java
+++ src/main/java/com/example/demo/autorizationandautofication/JwtUserDetailsService.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,7 @@ import com.example.demo.User;
 import com.example.demo.UserRepository;
 
 @Component
-public class JwtUserDetailsService{
+public class JwtUserDetailsService implements UserDetailsService{
 	
 	@Autowired
 	UserRepository userRepository;
